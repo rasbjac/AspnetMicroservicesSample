065dca4e88419470f390bb7e2208c2bd6b6657df
Rafael Santos <rasb.jac@gmail.com>
2022-07-22 17:55:30 -0300

    Starting WebApp.
    Starting WebApp.

---
+++ src/WebApps/AspnetRunBasics/Services/CatalogService.cs
@@ -0,0 +1,29 @@
+﻿using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        public Task<CatalogModel> CreateCatalog(CatalogModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<CatalogModel> GetCatalog(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
